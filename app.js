8c0e5f6141d133908ef052cf2f5529a1162875c9
Jonathan OReilly <jonathanoreilly@live.co.uk>
2023-06-14 08:47:46 +0100

    about me page

---
+++ app.js
@@ -0,0 +1,10 @@
+welcome = alert("Hello user welcome to my humble webpage. Lets get to know each other!");
+usersName = prompt("Please can you tell me your name?");
+
+favouriteAnimal = prompt("Nice to meet you " + usersName + ", my name is Johnny. What's your favourite animal?");
+if (favouriteAnimal === "cat"){
+    alert ("That's awesome, my favourite animal is also a cat")
+}
+else { (alert ("So your favourite animal is a " + favouriteAnimal + "! Thank you for sharing that with me. My favourite animal is a cat."))
+}
+console.log (usersName);
